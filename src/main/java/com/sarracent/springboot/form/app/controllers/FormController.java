d7cbcb4c47303f68a729edcc7aac8ae3ad57b1ea
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-04-03 01:49:47 -0300

    Refactorizando validaciones, agregando atributo object de thymeleaf

--- src/main/java/com/sarracent/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sarracent/springboot/form/app/controllers/FormController.java
@@ -7,8 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class FormController {
@@ -27,11 +25,6 @@ public class FormController {
         model.addAttribute("titulo", "Resultado Form");
 
         if(result.hasErrors()) {
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(err -> {
-                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
 
