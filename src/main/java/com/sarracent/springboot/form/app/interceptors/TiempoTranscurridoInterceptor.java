e05af5ef271de423bf7080bbeb092981ea56ea77
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-09 00:41:53 -0300

    89. Introduccón a los interceptores Http

---
+++ src/main/java/com/sarracent/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,23 @@
+package com.sarracent.springboot.form.app.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+}
