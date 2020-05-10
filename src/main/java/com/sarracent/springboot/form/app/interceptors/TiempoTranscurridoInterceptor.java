6c42b685b6e0ba0193096b38a8b609589de8d3bc
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-10 01:29:44 -0300

    93. Saliendo del interceptor en peticiones POST http - return en post handle

--- src/main/java/com/sarracent/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/sarracent/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -42,6 +42,10 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
                            ModelAndView modelAndView) throws Exception {
+        if (request.getMethod().equalsIgnoreCase("post")) {
+            return;
+        }
+
         long tiempoFin = System.currentTimeMillis();
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
         long tiempoTrascurrido = tiempoFin - tiempoInicio;
