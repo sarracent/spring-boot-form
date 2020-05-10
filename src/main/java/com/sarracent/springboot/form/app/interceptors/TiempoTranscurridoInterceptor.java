03eb8ccc742e3d1f1954e1932d43411905bd084a
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-10 00:51:09 -0300

    91. Registrando interceptor en la configuración de spring

--- src/main/java/com/sarracent/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/sarracent/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -3,6 +3,7 @@ package com.sarracent.springboot.form.app.interceptors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,14 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
 
-@Component
+@Component("tiempoTranscurridoInterceptor")
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod metodo = (HandlerMethod) handler;
+            logger.info("es un método del controlador: " + metodo.getMethod().getName());
+        }
+
         logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+        logger.info("Interceptando: " + handler);
         long tiempoInicio = System.currentTimeMillis();
         request.setAttribute("tiempoInicio", tiempoInicio);
 
@@ -35,7 +42,7 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
         long tiempoTrascurrido = tiempoFin - tiempoInicio;
 
-        if (modelAndView!= null) {
+        if (handler instanceof HandlerMethod && modelAndView!= null) {
             modelAndView.addObject("tiempoTrascurrido", tiempoTrascurrido);
         }
         logger.info("TiempoTranscurrido: " + tiempoTrascurrido + "ms");
